9049e5dfdd4b6114cc6b5791c9a0a500edbb3052
prashuhn25 <prashanthhn25@gmail.com>
2019-05-31 12:43:53 +0530

    initial commit

--- add.sh
+++ add.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-echo "enter the value of a"
-read a
-echo "enter the value of b"
-read b
-c
-c = 'expr $a + $b'
-echo "The sum of a and b is $c: "
+#echo "enter the value of a"
+#read a
+#echo "enter the value of b"
+#read b
+#echo "The sum of a and b is $c: "
+echo "x is $x = 100"
